3cee94e5b10badeec3e59180f566548909d46195
AlexKanotz <96555799+AlexKanotz@users.noreply.github.com>
2022-04-01 18:13:49 -0400

    Update MainActivity.java
    This is a test

--- app/src/main/java/edu/fsu/cs/wastenotwantnot/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // This is a test to make a pull request, by Alex Kanotz
     }
 }
