bf73d94b6bd8aa950de3996ce5e621366b91ea5c
Douglas Shirilla <douglasshirilla@yahoo.com>
2022-04-03 22:56:43 -0400

    added LoginFragment

--- app/src/main/java/edu/fsu/cs/wastenotwantnot/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginFragment fragment = new LoginFragment();
+        String tag = LoginFragment.class.getCanonicalName();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLogin, fragment, tag).commit();
     }
 }
 
