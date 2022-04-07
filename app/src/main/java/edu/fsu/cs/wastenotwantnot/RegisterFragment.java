d2e8360fb4dd03eadc9d115c9e60becc7e252931
L. Blake Wilson <blake.molde@gmail.com>
2022-04-06 22:10:15 -0500

    merged Alex's RegisterFragment.java

--- app/src/main/java/edu/fsu/cs/wastenotwantnot/RegisterFragment.java
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/RegisterFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +62,62 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        EditText firstName = view.findViewById(R.id.firstNameInput);
+        EditText lastName = view.findViewById(R.id.lastNameInput);
+        EditText emailAddress = view.findViewById(R.id.emailInput);
+        EditText address = view.findViewById(R.id.addressInput);
+        EditText username = view.findViewById(R.id.usernameInput);
+        EditText password = view.findViewById(R.id.passwordInput);
+        EditText confirmPassword = view.findViewById(R.id.confirmPasswordInput);
+
+        Button applyButton = view.findViewById(R.id.applyBtn);
+        Button resetButton = view.findViewById(R.id.resetBtn);
+
+        // applyButton on click listener
+        applyButton.setOnClickListener(v -> {
+            // Validate data has been entered
+            if (firstName.getText().toString().equals(""))
+                Toast.makeText(getActivity(), "First name is not entered", Toast.LENGTH_SHORT).show();
+            if (lastName.getText().toString().equals(""))
+                Toast.makeText(getActivity(), "Last name is not entered", Toast.LENGTH_SHORT).show();
+            if (emailAddress.getText().toString().equals(""))
+                Toast.makeText(getActivity(), "Email address is not entered", Toast.LENGTH_SHORT).show();
+            if (address.getText().toString().equals(""))
+                Toast.makeText(getActivity(), "Address is not entered", Toast.LENGTH_SHORT).show();
+            if (username.getText().toString().equals(""))
+                Toast.makeText(getActivity(), "Username is not entered", Toast.LENGTH_SHORT).show();
+            if (password.getText().toString().equals(""))
+                Toast.makeText(getActivity(), "Password is not entered", Toast.LENGTH_SHORT).show();
+            if (confirmPassword.getText().toString().equals(""))
+                Toast.makeText(getActivity(), "Confirm password", Toast.LENGTH_SHORT).show();
+
+            // Validate passwords are the same
+            String passwordStr = password.getText().toString();
+            String confirmPasswordStr = confirmPassword.getText().toString();
+            if (!passwordStr.equals(confirmPasswordStr))
+                Toast.makeText(getActivity(), "Passwords are not the same", Toast.LENGTH_SHORT).show();
+
+            // TODO: validate user is not already in the database
+
+
+
+            // TODO: insert user information to the database
+        });
+
+        // resetButton on click listener
+        resetButton.setOnClickListener(v -> {
+            // Clear EditTexts
+            firstName.setText("");
+            lastName.setText("");
+            emailAddress.setText("");
+            address.setText("");
+            username.setText("");
+            password.setText("");
+            confirmPassword.setText("");
+        });
+
+        return view;
     }
 }
