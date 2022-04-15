3ec79b1734c11f22ed151f4738beebc4ce92ef23
Douglas Shirilla <douglasshirilla@yahoo.com>
2022-04-15 18:36:34 -0400

    add Listing.java and ListingDao.java

---
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/Listing.java
@@ -0,0 +1,7 @@
+package edu.fsu.cs.wastenotwantnot;
+
+public class Listing {
+}
+
+
+//for listing database table
---
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/ListingDao.java
@@ -0,0 +1,6 @@
+package edu.fsu.cs.wastenotwantnot;
+
+public interface ListingDao {
+}
+
+//will be used for accessing listings table
