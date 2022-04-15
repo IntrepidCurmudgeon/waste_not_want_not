1c858d5a0c5451785455691120b429e9f19b0ae1
L. Blake Wilson <blake.molde@gmail.com>
2022-04-15 15:02:12 -0500

    created User Repository

---
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/UserRepository.java
@@ -0,0 +1,29 @@
+package edu.fsu.cs.wastenotwantnot;
+
+import android.app.Application;
+
+public class UserRepository {
+    private UserDao mUserDao;
+    // TODO: livedata (for updating data as it changes)
+    // e.g. private LiveData<List<Word>> mAllWords;
+
+    UserRepository(Application application) {
+        UserRoomDatabase db = UserRoomDatabase.getDatabase(application);
+        mUserDao = db.userDao();
+//        mAllWords = mWordDao.getAlphabetizedWords();
+    }
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+/*    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }*/
+
+    // You must call this on a non-UI thread or your app will throw an exception. Room ensures
+    // that you're not doing any long running operations on the main thread, blocking the UI.
+    void insert(User user) {
+        UserRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mUserDao.insert(user);
+        });
+    }
+}
