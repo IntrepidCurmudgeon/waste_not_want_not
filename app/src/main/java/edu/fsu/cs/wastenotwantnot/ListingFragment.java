e841bbaa38872ec59facca15d705b0225c2b7864
Douglas Shirilla <douglasshirilla@yahoo.com>
2022-04-15 18:56:13 -0400

    add ListingFragment.java and fragment_listing.xml

---
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/ListingFragment.java
@@ -0,0 +1,8 @@
+package edu.fsu.cs.wastenotwantnot;
+
+public class ListingFragment {
+}
+
+//used for fragment_listing to display listview of posts when first opening the app
+
+//add options menu to allow users to be able to login from this screen
