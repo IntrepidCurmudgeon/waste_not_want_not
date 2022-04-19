5a24cb9560bdafa5511407b9bac5f9ea46fe5c5f
L. Blake Wilson <blake.molde@gmail.com>
2022-04-19 14:51:41 -0500

    added loadUserByEmailOrUserName

--- app/src/main/java/edu/fsu/cs/wastenotwantnot/UserDao.java
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/UserDao.java
@@ -25,4 +25,7 @@ public interface UserDao {
 
     @Query("SELECT * FROM user WHERE user_name=(:userName) AND password=(:pwd)")
     User loadUserByUserName(String userName, String pwd);
+
+    @Query("SELECT * FROM user WHERE email=(:email) OR user_name=(:userName)")
+    User loadUserByEmailOrUserName(String email, String pwd);
 }
