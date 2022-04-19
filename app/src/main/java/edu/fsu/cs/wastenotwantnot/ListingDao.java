7f81e72d4427788df30daa783e4e2e80e5f40b35
AlexKanotz <96555799+AlexKanotz@users.noreply.github.com>
2022-04-19 17:58:47 -0400

    Update ListingDao.java

--- app/src/main/java/edu/fsu/cs/wastenotwantnot/ListingDao.java
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/ListingDao.java
@@ -1,7 +1,19 @@
 package edu.fsu.cs.wastenotwantnot;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+
+//@Dao
 public interface ListingDao {
 
+    /*
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insert(Listing listing);
+    */
 }
 
 //will be used for accessing listings table
+
+
+// TODO: made changes to ListingDao, UserRepository, UserRoomDatabase, WasteNotWantNotModel
