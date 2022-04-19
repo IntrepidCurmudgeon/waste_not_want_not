58f02cc1449c05976757ad91cbcbd924a880f20d
L. Blake Wilson <blake.molde@gmail.com>
2022-04-18 19:45:23 -0500

    added foreign key constraint to Listing

--- app/src/main/java/edu/fsu/cs/wastenotwantnot/Listing.java
+++ app/src/main/java/edu/fsu/cs/wastenotwantnot/Listing.java
@@ -3,12 +3,16 @@ package edu.fsu.cs.wastenotwantnot;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 
-@Entity(tableName = "listing")
-
+@Entity(tableName = "listing",
+        foreignKeys = @ForeignKey(entity = User.class,
+        parentColumns = "id",
+        childColumns = "user_id",
+        onDelete = ForeignKey.CASCADE))
 public class Listing {
 
     @PrimaryKey(autoGenerate = true)
